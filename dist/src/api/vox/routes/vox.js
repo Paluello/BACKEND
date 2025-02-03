7929e81b0e73c6c49d167e8c504bf6262eb7afc0
Palu17 <10946432@polimi.it>
2025-02-04 00:49:23 +0100

    test 2

--- dist/src/api/vox/routes/vox.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * vox router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::vox.vox');
